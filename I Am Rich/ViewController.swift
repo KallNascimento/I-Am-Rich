e557fda2c39e2957371229577905c0b52ec08a02
KallNascimento <iksn7@icloud.com>
2020-06-28 16:20:59 -0300

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Kauê Nascimento on 28/06/20.
+//  Copyright © 2020 Kauê Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
